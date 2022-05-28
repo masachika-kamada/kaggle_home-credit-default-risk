c7dd5b20c2f879782880abe69f29e6b77128adc7
masachika <masachika.kmd@gmail.com>
2022-05-28 21:45:58 +0900

    bureauに関するプログラムをファイル分け

--- LightGBM-with-Simple-Features.py
+++ LightGBM-with-Simple-Features.py
@@ -12,75 +12,10 @@ import lightgbm as lgb
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from lib.utils import timer, one_hot_encoder
-from lib.create_data import application_train_test
+from lib.create_data import application_train_test, bureau_and_balance
 
 dir_ref = "./csv-data"
 
-
-# Preprocess bureau.csv and bureau_balance.csv
-def bureau_and_balance(num_rows=None, nan_as_category=True):
-    bureau = pd.read_csv(dir_ref + '/bureau.csv', nrows=num_rows)
-    bb = pd.read_csv(dir_ref + '/bureau_balance.csv', nrows=num_rows)
-    bb, bb_cat = one_hot_encoder(bb, nan_as_category)
-    bureau, bureau_cat = one_hot_encoder(bureau, nan_as_category)
-
-    # Bureau balance: Perform aggregations and merge with bureau.csv
-    bb_aggregations = {'MONTHS_BALANCE': ['min', 'max', 'size']}
-    for col in bb_cat:
-        bb_aggregations[col] = ['mean']
-    bb_agg = bb.groupby('SK_ID_BUREAU').agg(bb_aggregations)
-    bb_agg.columns = pd.Index([e[0] + "_" + e[1].upper()
-                              for e in bb_agg.columns.tolist()])
-    bureau = bureau.join(bb_agg, how='left', on='SK_ID_BUREAU')
-    bureau.drop(['SK_ID_BUREAU'], axis=1, inplace=True)
-    del bb, bb_agg
-    gc.collect()
-
-    # Bureau and bureau_balance numeric features
-    num_aggregations = {
-        'DAYS_CREDIT': ['min', 'max', 'mean', 'var'],
-        'DAYS_CREDIT_ENDDATE': ['min', 'max', 'mean'],
-        'DAYS_CREDIT_UPDATE': ['mean'],
-        'CREDIT_DAY_OVERDUE': ['max', 'mean'],
-        'AMT_CREDIT_MAX_OVERDUE': ['mean'],
-        'AMT_CREDIT_SUM': ['max', 'mean', 'sum'],
-        'AMT_CREDIT_SUM_DEBT': ['max', 'mean', 'sum'],
-        'AMT_CREDIT_SUM_OVERDUE': ['mean'],
-        'AMT_CREDIT_SUM_LIMIT': ['mean', 'sum'],
-        'AMT_ANNUITY': ['max', 'mean'],
-        'CNT_CREDIT_PROLONG': ['sum'],
-        'MONTHS_BALANCE_MIN': ['min'],
-        'MONTHS_BALANCE_MAX': ['max'],
-        'MONTHS_BALANCE_SIZE': ['mean', 'sum']
-    }
-    # Bureau and bureau_balance categorical features
-    cat_aggregations = {}
-    for cat in bureau_cat:
-        cat_aggregations[cat] = ['mean']
-    for cat in bb_cat:
-        cat_aggregations[cat + "_MEAN"] = ['mean']
-
-    bureau_agg = bureau.groupby('SK_ID_CURR').agg(
-        {**num_aggregations, **cat_aggregations})
-    bureau_agg.columns = pd.Index(
-        ['BURO_' + e[0] + "_" + e[1].upper() for e in bureau_agg.columns.tolist()])
-    # Bureau: Active credits - using only numerical aggregations
-    active = bureau[bureau['CREDIT_ACTIVE_Active'] == 1]
-    active_agg = active.groupby('SK_ID_CURR').agg(num_aggregations)
-    active_agg.columns = pd.Index(
-        ['ACTIVE_' + e[0] + "_" + e[1].upper() for e in active_agg.columns.tolist()])
-    bureau_agg = bureau_agg.join(active_agg, how='left', on='SK_ID_CURR')
-    del active, active_agg
-    gc.collect()
-    # Bureau: Closed credits - using only numerical aggregations
-    closed = bureau[bureau['CREDIT_ACTIVE_Closed'] == 1]
-    closed_agg = closed.groupby('SK_ID_CURR').agg(num_aggregations)
-    closed_agg.columns = pd.Index(
-        ['CLOSED_' + e[0] + "_" + e[1].upper() for e in closed_agg.columns.tolist()])
-    bureau_agg = bureau_agg.join(closed_agg, how='left', on='SK_ID_CURR')
-    del closed, closed_agg, bureau
-    gc.collect()
-    return bureau_agg
 
 # Preprocess previous_applications.csv
 def previous_applications(num_rows=None, nan_as_category=True):
@@ -304,12 +239,13 @@ def display_importances(feature_importance_df_):
 def main(debug=False):
     num_rows = 10000 if debug else None
     df = application_train_test(num_rows)
-    # with timer("Process bureau and bureau_balance"):
-    #     bureau = bureau_and_balance(num_rows)
-    #     print("Bureau df shape:", bureau.shape)
-    #     df = df.join(bureau, how='left', on='SK_ID_CURR')
-    #     del bureau
-    #     gc.collect()
+
+    with timer("Process bureau and bureau_balance"):
+        bureau = bureau_and_balance(num_rows)
+        print("Bureau df shape:", bureau.shape)
+        df = df.join(bureau, how='left', on='SK_ID_CURR')
+        del bureau
+        gc.collect()
     # with timer("Process previous_applications"):
     #     prev = previous_applications(num_rows)
     #     print("Previous applications df shape:", prev.shape)
--- lib/create_data.py
+++ lib/create_data.py
@@ -49,3 +49,71 @@ def application_train_test(num_rows=None, nan_as_category=False):
     del test_df
     gc.collect()
     return df
+
+
+def bureau_and_balance(num_rows=None, nan_as_category=True):
+    """bureau.csvとbureau_balance.csvを加工"""
+
+    """まずデータを読み込む"""
+    bureau = pd.read_csv(dir_ref + '/bureau.csv', nrows=num_rows)
+    bb = pd.read_csv(dir_ref + '/bureau_balance.csv', nrows=num_rows)
+    bb, bb_cat = one_hot_encoder(bb, nan_as_category)
+    bureau, bureau_cat = one_hot_encoder(bureau, nan_as_category)
+
+    """bureauに関するデータを結合する"""
+    bb_aggregations = {'MONTHS_BALANCE': ['min', 'max', 'size']}
+    for col in bb_cat:
+        bb_aggregations[col] = ['mean']
+    bb_agg = bb.groupby('SK_ID_BUREAU').agg(bb_aggregations)
+    bb_agg.columns = pd.Index([e[0] + "_" + e[1].upper()
+                              for e in bb_agg.columns.tolist()])
+    bureau = bureau.join(bb_agg, how='left', on='SK_ID_BUREAU')
+    bureau.drop(['SK_ID_BUREAU'], axis=1, inplace=True)
+    del bb, bb_agg
+    gc.collect()
+
+    # Bureau and bureau_balance numeric features
+    num_aggregations = {
+        'DAYS_CREDIT': ['min', 'max', 'mean', 'var'],
+        'DAYS_CREDIT_ENDDATE': ['min', 'max', 'mean'],
+        'DAYS_CREDIT_UPDATE': ['mean'],
+        'CREDIT_DAY_OVERDUE': ['max', 'mean'],
+        'AMT_CREDIT_MAX_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM': ['max', 'mean', 'sum'],
+        'AMT_CREDIT_SUM_DEBT': ['max', 'mean', 'sum'],
+        'AMT_CREDIT_SUM_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM_LIMIT': ['mean', 'sum'],
+        'AMT_ANNUITY': ['max', 'mean'],
+        'CNT_CREDIT_PROLONG': ['sum'],
+        'MONTHS_BALANCE_MIN': ['min'],
+        'MONTHS_BALANCE_MAX': ['max'],
+        'MONTHS_BALANCE_SIZE': ['mean', 'sum']
+    }
+    # Bureau and bureau_balance categorical features
+    cat_aggregations = {}
+    for cat in bureau_cat:
+        cat_aggregations[cat] = ['mean']
+    for cat in bb_cat:
+        cat_aggregations[cat + "_MEAN"] = ['mean']
+
+    bureau_agg = bureau.groupby('SK_ID_CURR').agg(
+        {**num_aggregations, **cat_aggregations})
+    bureau_agg.columns = pd.Index(
+        ['BURO_' + e[0] + "_" + e[1].upper() for e in bureau_agg.columns.tolist()])
+    # Bureau: Active credits - using only numerical aggregations
+    active = bureau[bureau['CREDIT_ACTIVE_Active'] == 1]
+    active_agg = active.groupby('SK_ID_CURR').agg(num_aggregations)
+    active_agg.columns = pd.Index(
+        ['ACTIVE_' + e[0] + "_" + e[1].upper() for e in active_agg.columns.tolist()])
+    bureau_agg = bureau_agg.join(active_agg, how='left', on='SK_ID_CURR')
+    del active, active_agg
+    gc.collect()
+    # Bureau: Closed credits - using only numerical aggregations
+    closed = bureau[bureau['CREDIT_ACTIVE_Closed'] == 1]
+    closed_agg = closed.groupby('SK_ID_CURR').agg(num_aggregations)
+    closed_agg.columns = pd.Index(
+        ['CLOSED_' + e[0] + "_" + e[1].upper() for e in closed_agg.columns.tolist()])
+    bureau_agg = bureau_agg.join(closed_agg, how='left', on='SK_ID_CURR')
+    del closed, closed_agg, bureau
+    gc.collect()
+    return bureau_agg
