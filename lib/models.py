2ab6968cf8066342f349a1d483f8547c8f87c619
masachika <masachika.kmd@gmail.com>
2022-05-29 02:15:29 +0900

    models.pyの整形

--- lib/models.py
+++ lib/models.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import KFold, StratifiedKFold
 def kfold_lightgbm(df, num_folds, submission_file_name, stratified=False, debug=False):
     """特殊文字が混ざっているとエラーが出るので"""
     df = df.rename(columns=lambda x: re.sub('[^A-Za-z0-9_]+', '', x))
-    # Divide in training/validation and test data
+    """結合していたDataFrameを、TARGETの値の有無でtrainとtestに分割"""
     train_df = df[df['TARGET'].notnull()]
     test_df = df[df['TARGET'].isnull()]
     print(f"Starting LightGBM. Train shape: {train_df.shape}, test shape: {test_df.shape}")
@@ -25,26 +25,17 @@ def kfold_lightgbm(df, num_folds, submission_file_name, stratified=False, debug=
     gc.collect()
     # Cross validation model
     if stratified:
-        folds = StratifiedKFold(
-            n_splits=num_folds,
-            shuffle=True,
-            random_state=1001)
+        folds = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=1001)
     else:
         folds = KFold(n_splits=num_folds, shuffle=True, random_state=1001)
     # Create arrays and dataframes to store results
     oof_preds = np.zeros(train_df.shape[0])
     sub_preds = np.zeros(test_df.shape[0])
     feature_importance_df = pd.DataFrame()
-    feats = [
-        f for f in train_df.columns if f not in [
-            'TARGET',
-            'SK_ID_CURR',
-            'SK_ID_BUREAU',
-            'SK_ID_PREV',
-            'index']]
+    feats = [f for f in train_df.columns if f not in [
+             'TARGET', 'SK_ID_CURR', 'SK_ID_BUREAU', 'SK_ID_PREV', 'index']]
 
-    for n_fold, (train_idx, valid_idx) in enumerate(
-            folds.split(train_df[feats], train_df['TARGET'])):
+    for n_fold, (train_idx, valid_idx) in enumerate(folds.split(train_df[feats], train_df['TARGET'])):
         train_x, train_y = train_df[feats].iloc[train_idx], train_df['TARGET'].iloc[train_idx]
         valid_x, valid_y = train_df[feats].iloc[valid_idx], train_df['TARGET'].iloc[valid_idx]
 
@@ -67,17 +58,14 @@ def kfold_lightgbm(df, num_folds, submission_file_name, stratified=False, debug=
         clf.fit(train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)],
                 eval_metric='auc', callbacks=[early_stopping(stopping_rounds=200), lgb.log_evaluation(200)])
 
-        oof_preds[valid_idx] = clf.predict_proba(
-            valid_x, num_iteration=clf.best_iteration_)[:, 1]
-        sub_preds += clf.predict_proba(test_df[feats],
-                                       num_iteration=clf.best_iteration_)[:, 1] / folds.n_splits
+        oof_preds[valid_idx] = clf.predict_proba(valid_x, num_iteration=clf.best_iteration_)[:, 1]
+        sub_preds += clf.predict_proba(test_df[feats], num_iteration=clf.best_iteration_)[:, 1] / folds.n_splits
 
         fold_importance_df = pd.DataFrame()
         fold_importance_df["feature"] = feats
         fold_importance_df["importance"] = clf.feature_importances_
         fold_importance_df["fold"] = n_fold + 1
-        feature_importance_df = pd.concat(
-            [feature_importance_df, fold_importance_df], axis=0)
+        feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
         print('Fold %2d AUC : %.6f' % (n_fold + 1, roc_auc_score(valid_y, oof_preds[valid_idx])))
         del clf, train_x, train_y, valid_x, valid_y
         gc.collect()
